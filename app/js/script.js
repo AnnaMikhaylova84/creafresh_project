959da3af8360d9128b2d62eb6de7a349dac21a6c
kmykhailov <acidity84@gmail.com>
2018-10-21 04:14:03 +0300

    changes

--- app/js/script.js
+++ app/js/script.js
@@ -10,9 +10,7 @@ if (isIE) {
     head.appendChild(link);
     // alert('it is IE');
 }
-
 //  ------ end for IE -----
-
 
 var imageArray = document.querySelectorAll('.gallery_block')
     ,galleryBox = document.querySelector('.gallery_box')
@@ -34,27 +32,7 @@ function heightImage(){
 
 
 heightImage(); // for image size 1980*1080
-
-
-window.addEventListener('resize', function() {
-  heightMenuList();
-  if (galleryBox.offsetWidth !== galleryBoxWidth) {
-    heightImage();
-  }
-}, true);
-
-
-var heightMenuList = function() {
-
-  if (document.documentElement.clientWidth < 1025) {
-    // console.log(document.documentElement.clientWidth);
-    var heightMenu = mainMenu.offsetHeight
-       ,heightMenuFooter = mainMenuFooter.offsetHeight;
-
-   mainMenuNav.style.height = (heightMenu - heightMenuFooter) + 'px';
-  }
-
-}
+styleMenuMini(); // if menu height < 440px
 
 // open header menu
 
@@ -64,8 +42,39 @@ var openMenu = function(el) {
   if (el.classList.contains('open')) {
     document.querySelector('.menu').classList.add('open');
     heightMenuList ();
-
   } else {
     document.querySelector('.menu').classList.remove('open');
   }
 }
+
+// for heade menu HEight
+
+var heightMenuList = function() {
+  if (document.documentElement.clientWidth < 1025) {
+    // console.log(document.documentElement.clientWidth);
+    var heightMenu = mainMenu.offsetHeight
+       ,heightMenuFooter = mainMenuFooter.offsetHeight;
+    mainMenuNav.style.height = (heightMenu - heightMenuFooter) + 'px';
+  }
+}
+
+// if menu height < 440px
+
+function styleMenuMini() {
+  if (document.documentElement.clientHeight < 441) {
+    var head  = document.getElementsByTagName("head")[0];
+    var link  = document.createElement("link");
+    link.rel  = "stylesheet";
+    link.href = "css/menu_height.css";
+    head.appendChild(link);
+  }
+}
+
+
+window.addEventListener('resize', function() {
+  heightMenuList();
+  styleMenuMini();
+  if (galleryBox.offsetWidth !== galleryBoxWidth) {
+    heightImage();
+  }
+}, true);
